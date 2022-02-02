297c7df27321840484040071a5bcded110633eb7
dhachanamoothy <dhachanamoothy3@gmail.com>
2022-02-02 15:38:00 +0530

    postgres test

--- src/sequelize/config.ts
+++ src/sequelize/config.ts
@@ -3,7 +3,7 @@ export const Config = () => {
   console.log('Entered pg config');
   return {
     // uri: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`,
-    uri:process.env.DATABASE_URL + "?sslmode=require",
+    uri:process.env.DATABASE_URL + "?sslmode=disable",
     options: {
       dialect: 'postgres' as Dialect,
       benchmark: true,
