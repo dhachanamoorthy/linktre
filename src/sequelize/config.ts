e54a203cf64da68f54a1d8c3d81c7be6b5584c64
dhachanamoothy <dhachanamoothy3@gmail.com>
2022-02-02 15:28:46 +0530

    minor fix

--- src/sequelize/config.ts
+++ src/sequelize/config.ts
@@ -2,7 +2,8 @@ import { Dialect } from 'sequelize';
 export const Config = () => {
   console.log('Entered pg config');
   return {
-    uri: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`,
+    // uri: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`,
+    uri:process.env.DATABASE_URL + "?sslmode=require",
     options: {
       dialect: 'postgres' as Dialect,
       benchmark: true,
