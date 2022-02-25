13b3bb7f151e4e2881aba27ba715cdfd5915733b
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-02-25 18:19:00 +0530

    added api to get all trees of user

--- src/module/tree/models/tree.entity.ts
+++ src/module/tree/models/tree.entity.ts
@@ -1,48 +1,54 @@
 import {
-  AllowNull,
-  AutoIncrement,
-  Column,
-  DataType,
-  ForeignKey,
-  Model,
-  PrimaryKey,
-  Table,
+	AllowNull,
+	AutoIncrement,
+	Column,
+	DataType,
+	ForeignKey,
+	Model,
+	PrimaryKey,
+	Table,
 } from "sequelize-typescript";
 import { users } from "../../user/models/users.entity";
 @Table({
-  tableName: "trees",
+	tableName: "trees",
 })
 export class trees extends Model<trees> {
-  @PrimaryKey
-  @AllowNull(false)
-  @AutoIncrement
-  @Column({
-    type: DataType.BIGINT,
-  })
-  id: number;
+	@PrimaryKey
+	@AllowNull(false)
+	@AutoIncrement
+	@Column({
+		type: DataType.BIGINT,
+	})
+	id: number;
 
-  @ForeignKey(() => users)
-  @AllowNull(false)
-  @Column({
-    type: DataType.BIGINT,
-  })
-  user_id: number;
+	@ForeignKey(() => users)
+	@AllowNull(false)
+	@Column({
+		type: DataType.BIGINT,
+	})
+	user_id: number;
 
-  @AllowNull(false)
-  @Column({
-    type: DataType.NOW,
-  })
-  created_at: Date;
+	@AllowNull(false)
+	@Column({
+		type: DataType.NOW,
+	})
+	tree_name: String;
 
-  @AllowNull(true)
-  @Column({
-    type: DataType.DATE,
-  })
-  updated_at: Date;
+	@AllowNull(false)
+	@Column({
+		type: DataType.NOW,
+	})
+	created_at: Date;
 
-  @AllowNull(true)
-  @Column({
-    type: DataType.DATE,
-  })
-  deleted_at: Date;
+	@AllowNull(true)
+	@Column({
+		type: DataType.DATE,
+	})
+	updated_at: Date;
+
+	@AllowNull(true)
+	@Column({
+		type: DataType.DATE,
+	})
+	deleted_at: Date;
 }
--- src/module/tree/tree.controller.ts
+++ src/module/tree/tree.controller.ts
@@ -1,82 +1,97 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Logger,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Logger,
+	Param,
+	ParseIntPipe,
+	Patch,
+	Post,
 } from "@nestjs/common";
 import { ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger";
 import { CreateTreeRequestDto } from "./dto/CreateTreeRequest.dto";
 import { UpdateTreeRequestDto } from "./dto/UpdateTreeRequest.dto";
 import { TreeService } from "./tree.service";
 
-@ApiTags('Tree')
+@ApiTags("Tree")
 @Controller("tree")
 export class TreeController {
-  constructor(private readonly treeService: TreeService) {}
+	constructor(private readonly treeService: TreeService) {}
 
-  @Post()
-  @HttpCode(200)
-  @ApiOperation({
-    description: "Api to create tree",
-  })
-  async createTree(@Body() payload: CreateTreeRequestDto) {
-    try {
-      let result = await this.treeService.createTree(payload);
-      return result;
-    } catch (err) {
-      Logger.log(err);
-      throw new Error("Internal Server Error");
-    }
-  }
+	@Post()
+	@HttpCode(200)
+	@ApiOperation({
+		description: "Api to create tree",
+	})
+	async createTree(@Body() payload: CreateTreeRequestDto) {
+		try {
+			let result = await this.treeService.createTree(payload);
+			return result;
+		} catch (err) {
+			Logger.log(err);
+			throw new Error("Internal Server Error");
+		}
+	}
 
-  @Patch("/:id")
-  @HttpCode(200)
-  @ApiOperation({
-    description: "Api to update tree",
-  })
-  async updateTree(
-    @Param("id", ParseIntPipe) id: number,
-    @Body() payload: UpdateTreeRequestDto
-  ) {
-    try {
-      let result = await this.treeService.updateTree(id, payload);
-      return result;
-    } catch (err) {
-      throw new Error("Internal Server Error");
-    }
-  }
+	@Patch("/:id")
+	@HttpCode(200)
+	@ApiOperation({
+		description: "Api to update tree",
+	})
+	async updateTree(
+		@Param("id", ParseIntPipe) id: number,
+		@Body() payload: UpdateTreeRequestDto
+	) {
+		try {
+			let result = await this.treeService.updateTree(id, payload);
+			return result;
+		} catch (err) {
+			throw new Error("Internal Server Error");
+		}
+	}
 
-  @Get("/:id")
-  @HttpCode(200)
-  @ApiOperation({
-    description: "Api to get tree",
-  })
-  async getTree(@Param("id", ParseIntPipe) id: number) {
-    try {
-      let result = await this.treeService.getTree(id);
-      return result;
-    } catch (err) {
-      throw new Error("Internal Server Error");
-    }
-  }
+	@Get("all/:user_id")
+	@HttpCode(200)
+	@ApiOperation({
+		description: "Api to get all trees",
+	})
+	async getAllTree(@Param("user_id") user_id: number) {
+		try {
+			let result = await this.treeService.getAllTree(user_id);
+			return result;
+		} catch (err) {
+			Logger.error(err);
+			throw err;
+		}
+	}
 
-  @Delete("/:id")
-  @HttpCode(200)
-  @ApiOperation({
-    description: "Api to delete tree",
-  })
-  async deleteTree(@Param("id", ParseIntPipe) id: number) {
-    try {
-      let result = await this.treeService.deleteTree(id);
-      return result;
-    } catch (err) {
-      throw new Error("Internal Server Error");
-    }
-  }
+	@Get("/:id")
+	@HttpCode(200)
+	@ApiOperation({
+		description: "Api to get tree",
+	})
+	async getTree(@Param("id", ParseIntPipe) id: number) {
+		try {
+			let result = await this.treeService.getTree(id);
+			return result;
+		} catch (err) {
+			throw new Error("Internal Server Error");
+		}
+	}
+
+	@Delete("/:id")
+	@HttpCode(200)
+	@ApiOperation({
+		description: "Api to delete tree",
+	})
+	async deleteTree(@Param("id", ParseIntPipe) id: number) {
+		try {
+			let result = await this.treeService.deleteTree(id);
+			return result;
+		} catch (err) {
+			throw new Error("Internal Server Error");
+		}
+	}
 }
