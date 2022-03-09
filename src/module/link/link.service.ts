bf477a4cff7e10e7d1c12114e639223450067d80
dhachanamoothy <dhachanamoothy3@gmail.com>
2022-03-09 16:41:23 +0530

    added api to delete links

--- src/module/link/link.service.ts
+++ src/module/link/link.service.ts
@@ -20,4 +20,7 @@ export class LinkService {
   async updateLink(id: number, payload: any) {
     return this.linkDaoService.updateLink(id, payload);
   }
+ async deleteLink(id:number) {
+   return this.linkDaoService.deleteLink(id);
+ }
 }
